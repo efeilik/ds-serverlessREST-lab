ae3ea85e5fe3ea5c2b5e4618a2b71b47ad24759b
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-10-22 20:33:34 +0100

    Exercise: Delete movie endpoint

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -24,6 +24,19 @@ export class RestAPIStack extends cdk.Stack {
     
     // Functions 
 
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", 
+      {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/deleteMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    }
+    );
     const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", 
       {
       architecture: lambda.Architecture.ARM_64,
@@ -89,6 +102,7 @@ export class RestAPIStack extends cdk.Stack {
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
         moviesTable.grantReadWriteData(newMovieFn)
+        moviesTable.grantReadWriteData(deleteMovieFn);
 
         // REST API 
         const api = new apig.RestApi(this, "RestAPI", {
@@ -118,6 +132,10 @@ export class RestAPIStack extends cdk.Stack {
         moviesEndpoint.addMethod(
           "POST",
           new apig.LambdaIntegration(newMovieFn, { proxy: true })
+        );
+        movieEndpoint.addMethod(
+          "DELETE",
+          new apig.LambdaIntegration(deleteMovieFn, { proxy: true })
         );
       }
     }
