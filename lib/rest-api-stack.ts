537612c5337a3040347e3ff2ca34f0ae43d599e1
Efeİlik <efe.ilik@ug.bilkent.edu.tr>
2024-10-22 19:31:53 +0100

    Add API endpoint for posting a new movie.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -23,6 +23,20 @@ export class RestAPIStack extends cdk.Stack {
 
     
     // Functions 
+
+    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", 
+      {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+      }
+      );
     const getMovieByIdFn = new lambdanode.NodejsFunction(
       this,
       "GetMovieByIdFn",
@@ -74,7 +88,8 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
-        
+        moviesTable.grantReadWriteData(newMovieFn)
+
         // REST API 
         const api = new apig.RestApi(this, "RestAPI", {
           description: "demo api",
@@ -99,6 +114,10 @@ export class RestAPIStack extends cdk.Stack {
         movieEndpoint.addMethod(
           "GET",
           new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
+        );
+        moviesEndpoint.addMethod(
+          "POST",
+          new apig.LambdaIntegration(newMovieFn, { proxy: true })
         );
       }
     }
